f8ef7faa9536c2d5854743802e457a9e09e6c46b
Viktor Dohanyos <viktor_dohanyos@epam.com>
2021-09-22 18:21:29 +0200

    shared componenents: header, button, info, search created, index.ts updated

--- courses-app/src/app/shared/shared.module.ts
+++ courses-app/src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SearchComponent, InfoComponent, ButtonComponent, HeaderComponent } from './components/';
 
-
+// @TODO: It would be nice to find a solution not to use type 'any' here
+const components: any[] = [
+  HeaderComponent,
+  ButtonComponent,
+  InfoComponent,
+  SearchComponent
+]
 @NgModule({
-  declarations: [],
+  declarations: components,
   imports: [
     CommonModule
-  ]
+  ],
+  exports: components
 })
 export class SharedModule { }
