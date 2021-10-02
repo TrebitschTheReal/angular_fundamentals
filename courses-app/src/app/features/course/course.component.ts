5b42ffe8a72867f36aad1a5d226053f2f69b3733
Viktor Dohanyos <viktor_dohanyos@epam.com>
2021-10-02 23:11:08 +0200

    course add-edit implemented

--- courses-app/src/app/features/course/course.component.ts
+++ courses-app/src/app/features/course/course.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-course',
@@ -6,11 +7,23 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+  courseForm: FormGroup = new FormGroup({
+    'courseData': new FormGroup({
+      'title': new FormControl(''),
+      'desc': new FormControl(''),
+      'authorName': new FormControl(''),
+      'duration': new FormControl(''),
+    })
+  })
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+
   }
 
 }
--- courses-app/src/app/features/course/course.module.ts
+++ courses-app/src/app/features/course/course.module.ts
@@ -1,14 +1,21 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CourseComponent} from './course.component';
+import {SharedModule} from "../../shared/shared.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
   declarations: [
     CourseComponent
   ],
+  exports: [
+    CourseComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class CourseModule {
