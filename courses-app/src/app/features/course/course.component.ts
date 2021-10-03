9a81199ec370029dc54386d7f47eab62632f8f29
Viktor Dohanyos <viktor_dohanyos@epam.com>
2021-10-03 11:06:10 +0200

    course component refactored, minutes pipe improved

--- courses-app/src/app/features/course/course.component.ts
+++ courses-app/src/app/features/course/course.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-course',
@@ -20,9 +20,22 @@ export class CourseComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.courseForm = new FormGroup({
+      'courseData': new FormGroup({
+        'title': new FormControl(null, Validators.required),
+        'desc': new FormControl(null, Validators.required),
+        'duration': new FormControl(null, [
+          Validators.required,
+          Validators.minLength(0)]),
+      }),
+      'newAuthor': new FormGroup({
+        'authorName': new FormControl([]),
+      })
+    })
   }
 
   onSubmit() {
+    console.log(this.courseForm)
 
   }
 
--- courses-app/src/app/shared/pipes/minutes.pipe.ts
+++ courses-app/src/app/shared/pipes/minutes.pipe.ts
@@ -5,11 +5,12 @@ import {Pipe, PipeTransform} from "@angular/core";
 })
 export class MinutesPipe implements PipeTransform {
   transform(n: number): string {
-    let num = n;
-    let hours = (num / 60);
+    let hours = (n / 60);
     let rhours = Math.floor(hours);
     let minutes = (hours - rhours) * 60;
     let rminutes = Math.round(minutes);
-    return (rhours < 10 ? '0' + rhours : rhours) + " : " + (rminutes < 10 ? '0' + rminutes : rminutes) + " hours";
+
+    return isNaN(rminutes) || isNaN(rhours) ? '00 : 00 hours' :
+      ((hours < 10 ? '0' + rhours : rhours) + " : " + (rminutes < 10 ? '0' + rminutes : rminutes) + " hours");
   }
 }
